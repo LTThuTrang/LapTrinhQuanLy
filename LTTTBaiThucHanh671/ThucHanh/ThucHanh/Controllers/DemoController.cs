ef4a81f8e5df5685b1dfc2e4ab74809eba82d4a6
LeThuTrang12 <lethutrang290400@gmail.com>
2022-03-01 10:54:16 +0700

    commit

---
+++ LTTTBaiThucHanh671/ThucHanh/ThucHanh/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThucHanh.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string Masinhvien, string Hotensinhvien)
+        {
+            ViewBag.Message = Masinhvien + "" + Hotensinhvien;
+            return View();
+        }
+    }
+}
