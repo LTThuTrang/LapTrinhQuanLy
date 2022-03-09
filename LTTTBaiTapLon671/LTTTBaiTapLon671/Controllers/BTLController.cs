fd4e0f610711b8ddb9e74dc6a026ecea3fcb433e
LeThuTrang12 <lethutrang290400@gmail.com>
2022-03-09 12:27:21 +0700

    commit

---
+++ LTTTBaiTapLon671/LTTTBaiTapLon671/Controllers/BTLController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTTTBaiTapLon671.Controllers
+{
+    public class BTLController : Controller
+    {
+        // GET: BTL
+        public ActionResult Index(string TenDangNhap, string MatKhau)
+        {
+            ViewBag.Message = TenDangNhap + "" + MatKhau;
+            return View();
+        }
+    }
+}
