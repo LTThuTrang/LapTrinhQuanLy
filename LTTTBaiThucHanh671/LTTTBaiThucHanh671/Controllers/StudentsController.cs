de010bb44c5cd525a1b9685c5e57da4072f0c01e
LeThuTrang12 <lethutrang290400@gmail.com>
2022-04-07 10:31:38 +0700

    commit

---
+++ LTTTBaiThucHanh671/LTTTBaiThucHanh671/Controllers/StudentsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LTTTBaiThucHanh671.Models;
+using LTTTBaiThucHanh671.Models.Process;
+
+namespace LTTTBaiThucHanh671.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private ThuchanhDbcontext db = new ThuchanhDbcontext();
+        StringProcess aukey = new StringProcess();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            if (db.Students.Count() == 0)
+            {
+                ViewBag.NewSTDID = "STD01";
+            }
+            else
+            {
+                var STDID = db.Students.OrderByDescending(m => m.StudnetID).FirstOrDefault().StudnetID;
+                var newID = aukey.AutoGenerateID("STD", STDID);
+                ViewBag.NewSTDID = newID;
+            }
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudnetID,StudnetName")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StudnetID,StudnetName")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LTTTBaiThucHanh671/LTTTBaiThucHanh671/Models/Process/StringProcess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace LTTTBaiThucHanh671.Models.Process
+{
+    public class StringProcess
+    {
+        public string AutoGenerateID(string text, string ID)
+        {
+            string strKey = "";
+            string numPart = "", strPart = "", strPhanSo = "";
+
+            numPart = Regex.Match(ID, @"\d+").Value;
+            strPart = Regex.Match(ID, @"\D+").Value;
+
+            int phanso = (Convert.ToInt32(numPart) + 1);
+            for (int i = 0; i < numPart.Length - phanso.ToString().Length; i++)
+            {
+                strPhanSo += "0";
+            }
+            strPhanSo += phanso;
+            strKey = text + strPhanSo;
+
+            return strKey;
+
+        }
+    }
+}
