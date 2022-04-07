731c38702cce58b7a56f005ab4d5f38e945e35b7
LeThuTrang12 <lethutrang290400@gmail.com>
2022-04-07 10:55:58 +0700

    commit

---
+++ LTTTBaiTapLon671/LTTTBaiTapLon671/Controllers/AdminsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LTTTBaiTapLon671.Models;
+using LTTTBaiTapLon671.Models.Process;
+
+namespace LTTTBaiTapLon671.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private BTLDbContext db = new BTLDbContext();
+        StringProcess aukey = new StringProcess();
+
+        // GET: Admins
+        public ActionResult Index()
+        {
+            return View(db.Admins.ToList());
+        }
+
+        // GET: Admins/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // GET: Admins/Create
+        public ActionResult Create()
+        {
+          if (db.Admins.Count() == 0)
+            {
+                ViewBag.NewADMID = "ADM01";
+            }
+            else
+            {
+                var ADMID = db.Admins.OrderByDescending(m => m.AdminID).FirstOrDefault().AdminID;
+                var newID = aukey.AutoGenerateID("ADM", ADMID);
+                ViewBag.NewADMID = newID;
+            }
+
+                return View();
+        }
+
+        // POST: Admins/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AdminID,AdminName")] Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Admins.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(admin);
+        }
+
+        // GET: Admins/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Admins/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AdminID,AdminName")] Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(admin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(admin);
+        }
+
+        // GET: Admins/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Admin admin = db.Admins.Find(id);
+            db.Admins.Remove(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
